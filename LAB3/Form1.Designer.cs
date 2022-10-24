7d869f404ed40b3106f372946c4f2b52caaff720
piekihere <igopie@st.ext.amu.edu.pl>
2022-10-24 23:54:44 +0200

    misc

--- LAB3/Form1.Designer.cs
+++ LAB3/Form1.Designer.cs
@@ -42,6 +42,7 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Tytuł,
