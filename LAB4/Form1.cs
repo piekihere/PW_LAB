be6a09298741d38e992c708bac5624893945fe76
piekihere <igopie@st.ext.amu.edu.pl>
2022-11-07 17:25:13 +0100

    catch black

--- LAB4/Form1.cs
+++ LAB4/Form1.cs
@@ -11,5 +11,51 @@ namespace LAB4
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();  
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(open.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            //bmp.GetPixel(i,j).R == 0 && bmp.GetPixel(i, j).G==0 && bmp.GetPixel(i, j).B ==0
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for(int j = 0; j < bmp.Height; j++)
+                {
+                    if(bmp.GetPixel(i,j).GetBrightness() < 0.05)
+                    {
+                        bmp.SetPixel(i, j, Color.White);
+                    }
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            //bmp.GetPixel(i,j).R == 0 && bmp.GetPixel(i, j).G==0 && bmp.GetPixel(i, j).B ==0
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    if (bmp.GetPixel(i, j).GetBrightness() < 0.05)
+                    {
+                        bmp.SetPixel(i, j, Color.White);
+                    }
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+    }
     }
 }
