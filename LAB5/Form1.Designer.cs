2589255d5251e7a8a593d844b58e21c81322cc4a
piekihere <igopie@st.ext.amu.edu.pl>
2022-11-14 19:09:49 +0100

    skonczone

--- LAB5/Form1.Designer.cs
+++ LAB5/Form1.Designer.cs
@@ -28,20 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wczytaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Wylicz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(396, 242);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 111);
+            this.listView1.Margin = new System.Windows.Forms.Padding(1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 169);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(647, 319);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private ListView listView1;
+        public String seq="";
+        public Dictionary<string, int> dictOfSeqs = new Dictionary<string, int>();
     }
 }
--- LAB5/Form1.cs
+++ LAB5/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace LAB5
 {
     public partial class Form1 : Form
@@ -8,6 +10,41 @@ namespace LAB5
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = File.ReadAllText(open.FileName);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < textBox1.Text.ToString().Length - 3; i++)
+            {
+                if(dictOfSeqs.ContainsKey(textBox1.Text.ToString().Substring(i, 4)))
+                {
+                    dictOfSeqs[textBox1.Text.ToString().Substring(i, 4)] += 1;
+                }
+                else
+                {
+                    dictOfSeqs[textBox1.Text.ToString().Substring(i, 4)] = 1;
+                }
+            }
+            foreach (KeyValuePair<string, int> kvp in dictOfSeqs)
+            {
+                String wpis = kvp.Key +" : "+ kvp.Value.ToString();
+                listView1.Items.Add(wpis);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
