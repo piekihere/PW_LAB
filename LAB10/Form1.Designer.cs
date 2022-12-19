2e1c06b5d1229a97db730079ca68834f040d72e3
piekihere <rogi.rzakeip@gmail.com>
2022-12-19 20:27:08 +0100

    init

--- LAB10/Form1.Designer.cs
+++ LAB10/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace LAB10
+﻿using System.Media;
+
+namespace LAB10
 {
     partial class Form1
     {
@@ -31,9 +33,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.christmasCarol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -54,6 +59,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Pause";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -63,19 +69,12 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Stop";
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // listView1
-            // 
-            this.listView1.Location = new System.Drawing.Point(139, 12);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(246, 236);
-            this.listView1.TabIndex = 3;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(139, 265);
+            this.checkBox1.Location = new System.Drawing.Point(96, 228);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(67, 19);
             this.checkBox1.TabIndex = 4;
@@ -84,7 +83,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(264, 265);
+            this.button4.Location = new System.Drawing.Point(414, 228);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(121, 23);
             this.button4.TabIndex = 5;
@@ -92,19 +91,47 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.christmasCarol,
+            this.ID});
+            this.dataGridView1.Location = new System.Drawing.Point(96, 13);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(439, 210);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // christmasCarol
+            // 
+            this.christmasCarol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.christmasCarol.HeaderText = "Kolęda";
+            this.christmasCarol.Name = "christmasCarol";
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(397, 303);
+            this.ClientSize = new System.Drawing.Size(547, 262);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.listView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,8 +142,12 @@
         private Button button1;
         private Button button2;
         private Button button3;
-        private ListView listView1;
         private CheckBox checkBox1;
         private Button button4;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn christmasCarol;
+        private DataGridViewTextBoxColumn ID;
+        public Dictionary<string, string> MusicVault = new Dictionary<string, string>();
+        public SoundPlayer MusicPlayer = new SoundPlayer();
     }
 }
--- LAB10/Form1.cs
+++ LAB10/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace LAB10
 {
     public partial class Form1 : Form
@@ -10,11 +12,56 @@ namespace LAB10
         private void button1_Click(object sender, EventArgs e)
         {
 
+            if (dataGridView1.Rows.Count > 0 && dataGridView1.SelectedRows.Count > 0)
+            {
+                if (checkBox1.Checked)
+                {
+                    Random rnd = new Random();
+                    int keyNumber = rnd.Next(0, MusicVault.Count());
+                    
+                }
+                foreach (KeyValuePair<string, string> track in MusicVault)
+                {
+                    if (track.Key == dataGridView1.SelectedRows[0].Cells[1].Value.ToString())
+                    {
+                        MusicPlayer.SoundLocation = track.Value;
+                        MusicPlayer.Play();
+                        break;
+                    }
+                }
+
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
+            ofd.CheckFileExists = true;
+            ofd.Filter = "WAV files (*.wav)|*.wav";
+            ofd.DefaultExt = ".wav";
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string[] names = ofd.SafeFileNames;
+                string[] paths = ofd.FileNames;
+                for( int i=0 ; i < names.Length; i++)
+                {
+                    string ID = Guid.NewGuid().ToString();
+                    dataGridView1.Rows.Add(names[i], ID);
+                    MusicVault.Add(ID, paths[i]);
+                }
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MusicPlayer.Stop();
         }
     }
 }
